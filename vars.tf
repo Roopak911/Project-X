2ef84c18b8be9ff57ee167018f0c8698558659ec
Roopak911 <135810184+Roopak911@users.noreply.github.com>
2023-11-03 22:40:38 +0530

    Update vars.tf with variables for public subnets

--- vars.tf
+++ vars.tf
@@ -9,3 +9,9 @@ variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16"
 }
+
+variable "public_subnets_cidr" {
+  type        = list
+  description = "CIDR block for Public Subnet"
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  }
