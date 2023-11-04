09280f2b62478031e50f6386f9c2cf6636f0657e
Roopak911 <roopaktelharkkar@yahoo.com>
2023-11-04 16:31:13 +0530

    Updated code to create NAT gateway and assign it to the public subnet.

--- main.tf
+++ main.tf
@@ -65,3 +65,21 @@ resource "aws_internet_gateway" "ig" {
     Name = "${var.environment}-igw"
   }
 }
+
+
+# Elastic-IP (eip) for NAT
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+
+# NAT
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = element(aws_subnet.public_subnet.*.id, 1)
+
+  tags = {
+    Name = "nat"
+  }
+}
+
