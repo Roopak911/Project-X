8e80eed4f95480e0a700523e861b93889c221d52
Roopak911 <roopaktelharkkar@yahoo.com>
2023-11-04 15:44:07 +0530

    Updated main.tf and vars .tf to create public subnets.

--- main.tf
+++ main.tf
@@ -22,3 +22,16 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+# Public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count                   = length(var.public_subnets_cidr)
+  cidr_block              = element(var.public_subnets_cidr, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-${element(var.public_subnets_cidr, count.index)}-public-subnet"
+  }
+}
+
