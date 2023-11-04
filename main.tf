095217c742bf5ecbc1310b90cac710cc00d4a212
Roopak911 <roopaktelharkkar@yahoo.com>
2023-11-04 16:56:36 +0530

    Updated code in main.tf to add routes in private route table.

--- main.tf
+++ main.tf
@@ -110,3 +110,9 @@ resource "aws_route" "public_internet_gateway" {
 }
 
 
+# Route for NAT
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.privateRT.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
