1ee7dd7fc3cf28ebfe3cf4418d8d4f3ce3ef8137
Roopak911 <roopaktelharkkar@yahoo.com>
2023-11-05 12:27:47 +0530

    Updated main.tf for public route table subnet association.

--- main.tf
+++ main.tf
@@ -116,3 +116,13 @@ resource "aws_route" "private_nat_gateway" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat.id
 }
+
+
+# Subnet association for public route table
+resource "aws_route_table_association" "publicRTassoc" {
+  subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+
+
