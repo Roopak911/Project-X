cc3df46aa3a8a0de816520f1142d37b6acebd6be
Roopak911 <roopaktelharkkar@yahoo.com>
2023-11-05 23:58:34 +0530

    Updated repo by adding codes for vpc, sg and an instance creation modules.

--- ec2_module/ec2.tf
+++ ec2_module/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "new" {
   subnet_id              = var.subnet-id
   key_name               = "jake-new-practice-key-pair"
   vpc_security_group_ids = var.sg_id
-  user_data              = file("user-data.sh")
+  user_data              = file("./ec2_module/user-data.sh")
   tags = {
     Name = "${var.environment}-Bastion-Host-with-NginX"
   }
