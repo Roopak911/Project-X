ccfe5b9dae93a1ed4355592630bc4443dd85af37
Roopak911 <roopaktelharkkar@yahoo.com>
2023-11-06 00:18:39 +0530

    Updated repo by adding codes for vpc, sg and an instance creation modules.

--- ec2_module/ec2.tf
+++ ec2_module/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "new" {
   ami                    = var.ami-id
   instance_type          = var.instance-type
   subnet_id              = var.subnet-id
-  key_name               = "jake-new-practice-key-pair"
+  key_name               = "my-new-us-key"
   vpc_security_group_ids = [var.sg_id]
   user_data              = file("./ec2_module/user-data.sh")
   tags = {
