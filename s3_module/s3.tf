4c1362cfee77785b7488a54fead16681ad596c7d
Roopak911 <roopaktelharkkar@yahoo.com>
2023-11-06 18:17:46 +0530

    Updated repo by adding codes for vpc, sg, s3 object uploading into bucket and an instance creation modules.

--- s3_module/s3.tf
+++ s3_module/s3.tf
@@ -8,7 +8,7 @@ resource "null_resource" "upload_files" {
   depends_on = [aws_s3_bucket.b]
   provisioner "local-exec" {
   command = <<EOT
-  aws s3 cp ./uploads s3://${aws_s3_bucket.b.bucket}/ --recursive
+  aws s3 cp ./s3_module/uploads s3://${aws_s3_bucket.b.bucket}/ --recursive
   EOT
   }
 }
