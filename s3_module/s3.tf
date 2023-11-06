6a5c33673f41d5a22335badd3c98c82265c08de5
Roopak911 <roopaktelharkkar@yahoo.com>
2023-11-06 16:27:17 +0530

    Updated repo by adding codes for vpc, sg, s3 object uploading into bucket and an instance creation modules.

--- s3_module/s3.tf
+++ s3_module/s3.tf
@@ -1,6 +1,19 @@
 resource "aws_s3_bucket" "b" {
   bucket = var.bucket_name
-  acl = var.acl_value
+  acl    = var.acl_value
+}
+
+
+# Creating an S3 object
+resource "aws_s3_object" "object" {
+  bucket     = aws_s3_bucket.b.id
+  for_each   = fileset("uploads/", "*")
+  key        = each.value
+  source     = "uploads/${each.value}"
+  etag       = filemd5("uploads/${each.value}")
+  depends_on = [
+    aws_s3_bucket.b
+  ]
 }
 
   
